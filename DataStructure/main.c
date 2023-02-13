c239254b560a9a9f170e661419ccb649ad36185e
naru <rlck1998@naver.com>
2023-02-13 20:15:05 +0900

    Initial Commit

---
+++ DataStructure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by 김원정 on 2023/02/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
